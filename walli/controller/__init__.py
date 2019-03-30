311eb3fa911ffe00b5062fb5fb41f59aa1264aea
querdos <hamza.essayegh@protonmail.com>
2019-03-30 19:15:28 +0100

    Add namespace and prefix to api definition

--- walli/controller/__init__.py
+++ walli/controller/__init__.py
@@ -3,9 +3,14 @@ from flask_restplus import Api
 from flask import Blueprint
 from ..config import API_VERSION
 
+from .about_controller import api as about_api
+
 blueprint = Blueprint('api', __name__)
 
 api = Api(blueprint,
           title='Walli API',
           version=API_VERSION,
-          description='An api for managing your linux firewall (iptables)')
+          description='An api for managing your linux firewall (iptables)',
+          prefix='/api')
+
+api.add_namespace(about_api, path='/about')
