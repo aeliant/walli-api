78de06e17a6c576a4062620cfc0cf15b9dac4ac7
querdos <hamza.essayegh@protonmail.com>
2019-03-30 19:15:14 +0100

    Rename manage to app

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 """Walli application entry point."""
 import os
 from walli import create_app
+from walli.controller import blueprint
 
 app = create_app(os.getenv('FLASK_ENV', 'dev'))
+app.register_blueprint(blueprint)
+
 app.app_context().push()
