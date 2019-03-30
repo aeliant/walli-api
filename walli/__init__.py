fa73581ba01ecb68323d2623f0ccd59ca556b0d7
querdos <hamza.essayegh@protonmail.com>
2019-03-30 15:25:50 +0100

    Update main package entrypoint

---
+++ walli/__init__.py
@@ -0,0 +1,9 @@
+"""Main package entry point."""
+from flask import Flask
+from .config import config_by_name
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_by_name.get(config_name))
+
+    return app
