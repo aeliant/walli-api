7c004d64262311406dd11cc0e2e4eb96049e8fca
querdos <hamza.essayegh@protonmail.com>
2019-04-06 11:03:51 +0200

    Add DTO for tables

--- walli_api/utils/dto.py
+++ walli_api/utils/dto.py
@@ -3,7 +3,13 @@ from flask_restplus import Namespace, fields
 
 
 class AboutDto:
+    """About endpoint data transfert object for marshalling."""
+
     api = Namespace('about', description='About related operation')
     about = api.model('about', {
         'version': fields.String(description='The api version.')
     })
+
+class TableDto:
+    """Table endpoint data transfert objects for marshalling and documenting."""
+    api = Namespace('table', description='Table related operation')
