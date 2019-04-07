486eeaa163d84c2e9d59ad8a6a454f83737915fc
querdos <hamza.essayegh@protonmail.com>
2019-04-07 18:05:49 +0200

    Add exception for not found chains

--- walli_api/utils/exceptions.py
+++ walli_api/utils/exceptions.py
@@ -4,3 +4,7 @@
 class TableNotFoundException(Exception):
     """Exception that must be raised when a table has not been found."""
     pass
+
+class ChainNotFoundException(Exception):
+    """Exception that must be raised when a table has not been found."""
+    pass
