40779c1de0999be501a62c69440437d39fa78336
querdos <hamza.essayegh@protonmail.com>
2019-04-07 16:36:19 +0200

    Add tests for tables endpoint

---
+++ tests/test_tables.py
@@ -0,0 +1,20 @@
+"""Functionnal tests for the tables endpoint."""
+from config import URL
+
+import requests
+
+def test_tables_collection():
+    """Checks validity of the tables collection."""
+    response = requests.get('%s/tables' % URL)
+
+    assert 200 == response.status_code
+    assert isinstance(response.json(), list)
+    assert 5 == len(response.json())
+    assert {
+        'filter',
+        'nat',
+        'mangle',
+        'raw',
+        'nat',
+        'security'
+    } == set(response.json())
